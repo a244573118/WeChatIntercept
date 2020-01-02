c0537da61a8b65f00b2afb360f33a6f09eb3423a
a244573118 <244573118@qq.com>
2020-01-02 20:00:33 +0800

    支持微信版本2.3.29消息防撤回

--- Install.sh
+++ Install.sh
@@ -18,11 +18,11 @@ framework_name="WeChatIntercept"
 app_bundle_path="${WeChat_path}/Contents/MacOS"
 app_executable_path="${app_bundle_path}/${app_name}"
 app_executable_backup_path="${app_executable_path}_backup"
-framework_path="${shell_path}/${framework_name}.framework"
+framework_path="${app_bundle_path}/${framework_name}.framework"
 if [ ! -w "$WeChat_path" ]
 then
     echo -e "为了将ZY助手写入微信, 请输入密码 ： "
-    sudo chown -R $(whoami) "$WeChat_path"
+    sudo chmod -R 777 $(whoami) "$WeChat_path"
 fi
 
 if [ ! -f "$app_executable_backup_path" ] || [ -n "$1" -a "$1" = "--force" ]
@@ -35,7 +35,7 @@ else
 fi
 
 if [[ "$result" == 'y' ]]; then
-    cp -r $framework_path ${app_bundle_path}
+    cp -r "${shell_path}/${framework_name}.framework" ${app_bundle_path}
     ${shell_path}/insert_dylib --all-yes "${framework_path}/${framework_name}" "$app_executable_backup_path" "$app_executable_path"
     echo "安装成功！"
 fi
--- WeChatIntercept.framework/Versions/A/Headers/WeChatIntercept.h
+++ WeChatIntercept.framework/Versions/A/Headers/WeChatIntercept.h
@@ -55,7 +55,7 @@ FOUNDATION_EXPORT const unsigned char WeChatInterceptVersionString[];
     @end
 @interface MessageService : NSObject
 - (void)onRevokeMsg:(id)arg1;
-- (void)FFToNameFavChatZZ:(id)arg1;
+- (void)FFToNameFavChatZZ:(id)arg1 sessionMsgList:(id)arg2;
 - (void)OnSyncBatchAddMsgs:(NSArray *)arg1 isFirstSync:(BOOL)arg2;
 - (void)FFImgToOnFavInfoInfoVCZZ:(id)arg1 isFirstSync:(BOOL)arg2;
 - (id)SendTextMessage:(id)arg1 toUsrName:(id)arg2 msgText:(id)arg3 atUserList:(id)arg4;
